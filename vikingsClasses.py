9a386ea888a6ad58d38849c2c4879bf4c5d899e6
Rodrigamen <roderic.f9@gmail.com>
2022-01-28 23:22:54 +0100

    regular :(

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -2,23 +2,110 @@
 # Soldier
 
 
+from unicodedata import name
+
+
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strenght = strength
+
+    def attack (self):
+        return self.strength
+
+    def receiveDamage (self, damage):
+        self.damage = damage
+        self.health -= damage
+
+
+
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(self, health, strength)
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage, name):
+        self.damage = damage
+        self.health -= damage
+        if self.health > 0:
+            return f"{name} has received {damage} points of damage"
+        else:
+            return f"{name} has died in act of combat"
+   
+    def battleCry(self):
+        return "ODIN OWNS YOU ALL"
+
+    
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(self, health, strength)
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health -= damage
+        if self.health > 0:
+            return f"A saxon has received {damage} points of damage"
+        else:
+            return "A saxon has died in combat"
+
+
+
 
 # War
 
 
-class War:
-    pass
+class War(Viking, Saxon):
+    def __init__(self, name, health, strength):
+        super().__init__(self, health, strength)
+
+    vikingArmy = ""
+    saxonArmy = ""   
+
+    def addViking(Viking):
+        Viking()
+
+    def addSaxon(Saxon):
+        Saxon()
+
+    def vikingAttack(): 
+        Saxon.receiveDamage(damage) -= attack(Viking)
+
+    def saxonAttack():
+        Viking.receiveDamage(damage, name)-= attack(Saxon)
+
+    def showSatus():
+        if saxonArmy == "":
+            return "Vikings have won the war of the century"  
+        elif vikingArmy == "":
+            return "Saxons have won the war of the century"
+        elif saxonArmy != "" and vikingArmy != "":
+            return "Vikings and Saxons are still in the thick of battle"
+            
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
