c58d276a84343d2cba7f802a506a5e65d5ab9017
Monica Goizueta <monicagoizueta@hotmail.com>
2022-01-29 14:01:26 +0100

    cambios1

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -23,14 +23,16 @@ class Viking (Soldier):
         
     def __init__(self, nombre, health, strength):
         super().__init__(health, strength)
+
+        self.nombre= nombre
         
     def receivedamage (self):
         receivedamage= int(input("cuantos damage has recibido"))
         self.health -= receivedamage
         if self.health > 0:
-            return f'{nombre} has received {receivedamage} points of damage'
+            return f'{self.nombre} has received {receivedamage} points of damage'
         if self.health < 0:
-            return f'{nombre} has died in act of combat'
+            return f'{self.nombre} has died in act of combat'
     
     def battleCry (self):
         return "Odin Owns You All!"
